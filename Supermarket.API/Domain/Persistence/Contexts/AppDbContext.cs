a91e89849298a7b5efe4bf669e3bc656e14387da
zaj-e <carbonelmarcelo@gmail.com>
2020-09-01 11:03:27 -0500

    Services, Repositories and AppDbContext

---
+++ Supermarket.API/Domain/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domain.Persistence.Contexts
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Category>().ToTable("categories");
+            builder.Entity<Category>().HasKey(p => p.Id);
+        }
+    }
+}
